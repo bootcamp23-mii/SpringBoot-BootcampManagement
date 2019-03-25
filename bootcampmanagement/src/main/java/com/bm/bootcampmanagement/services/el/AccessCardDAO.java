95885b4b0e745490150fd23f5594dc38d17bcb7a
Firsta Septiana <restaarayaning@gmail.com>
2019-03-25 12:32:17 -0700

    dao yang kemarin kehapus maaf...

---
+++ bootcampmanagement/src/main/java/com/bm/bootcampmanagement/services/el/AccessCardDAO.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bm.bootcampmanagement.services.el;
+
+import com.bm.bootcampmanagement.entities.Accesscard;
+import com.bm.bootcampmanagement.entities.Locker;
+import com.bm.bootcampmanagement.repository.el.AccessCardRepository;
+import com.bm.bootcampmanagement.servicesInterface.el.AccessCardDAOInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Firsta
+ */
+@Service
+public class AccessCardDAO implements AccessCardDAOInterface{
+
+    @Autowired
+    AccessCardRepository accessCardRepository;
+
+    @Override
+    public Iterable<Accesscard> findAll() {
+        return accessCardRepository.findAll();
+    }
+
+    @Override
+    public Accesscard saveAccessCard(Accesscard accesscard) {
+        return accessCardRepository.save(accesscard);
+    }
+
+    @Override
+    public void deleteAccessCardById(String id) {
+       accessCardRepository.deleteById(id);
+    }
+   
+    
+}
---
+++ bootcampmanagement/src/main/java/com/bm/bootcampmanagement/services/el/VillageDAO.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bm.bootcampmanagement.services.el;
+
+import com.bm.bootcampmanagement.entities.Village;
+import com.bm.bootcampmanagement.repository.el.VillageRepository;
+import com.bm.bootcampmanagement.servicesInterface.el.VillageDAOInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Firsta
+ */
+@Service
+public class VillageDAO implements VillageDAOInterface{
+    
+    @Autowired
+    VillageRepository vr;
+    
+    @Override
+    public Iterable<Village> findAll() {
+        return vr.findAll();
+    }
+
+    @Override
+    public Village saveVillage(Village village) {
+        return vr.save(village);
+    }
+
+    @Override
+    public void deleteVillageById(String id) {
+       vr.deleteById(id);
+    }
+    
+}
