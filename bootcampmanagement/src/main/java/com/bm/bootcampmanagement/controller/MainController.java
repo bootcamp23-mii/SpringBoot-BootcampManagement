a86b42e77fa50c7b35184a5bfbe6c5625ffb1aa9
lestarilusiana <10lestarilusiana@gmail.com>
2019-03-26 09:39:20 +0700

    apa ini

--- bootcampmanagement/src/main/java/com/bm/bootcampmanagement/controller/MainController.java
+++ bootcampmanagement/src/main/java/com/bm/bootcampmanagement/controller/MainController.java
@@ -5,10 +5,14 @@
  */
 package com.bm.bootcampmanagement.controller;
 
+import com.bm.bootcampmanagement.entities.Achievement;
 import com.bm.bootcampmanagement.entities.Batchclass;
 import com.bm.bootcampmanagement.entities.District;
+import com.bm.bootcampmanagement.entities.Educationhistory;
 import com.bm.bootcampmanagement.entities.Employee;
+import com.bm.bootcampmanagement.entities.Employeecertification;
 import com.bm.bootcampmanagement.entities.Employeerole;
+import com.bm.bootcampmanagement.entities.Organization;
 import com.bm.bootcampmanagement.entities.Participant;
 import com.bm.bootcampmanagement.entities.Religion;
 import com.bm.bootcampmanagement.entities.Village;
@@ -19,9 +23,13 @@ import com.bm.bootcampmanagement.services.EmployeeDAO;
 import com.bm.bootcampmanagement.services.UploadFileResponse;
 import com.bm.bootcampmanagement.services.bm.BatchclassDAO;
 import com.bm.bootcampmanagement.services.bm.ParticipantDAO;
+import com.bm.bootcampmanagement.services.cv.AchievementDAO;
+import com.bm.bootcampmanagement.services.cv.EducationHistoryDAO;
+import com.bm.bootcampmanagement.services.cv.EmployeeCertificationDAO;
 import com.bm.bootcampmanagement.services.cv.EmployeeLanguageDAO;
 import com.bm.bootcampmanagement.services.cv.EmployeeRoleDAO;
 import com.bm.bootcampmanagement.services.cv.EmployeeSkillDAO;
+import com.bm.bootcampmanagement.services.cv.OrganizationDAO;
 import com.bm.bootcampmanagement.services.cv.ReligionDAO;
 import com.bm.bootcampmanagement.services.cv.WorkExperienceDAO;
 import com.bm.bootcampmanagement.services.el.DistrictDAO;
@@ -77,12 +85,21 @@ public class MainController {
     @Autowired
     DistrictDAO daoLD;
     @Autowired
+    AchievementDAO adao;
+    @Autowired
+    OrganizationDAO odao;
+    @Autowired
+    EducationHistoryDAO edao;
+
+    @Autowired
+    EmployeeCertificationDAO cdao;
+    @Autowired
     private static Logger log = LoggerFactory.getLogger(MainController.class);
     private SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
 
     @Autowired
     private MailService mailService;
-    
+
     @Autowired
     private DBFileStorageService DBFileStorageService;
 
@@ -93,7 +110,7 @@ public class MainController {
 
     @PostMapping("/login")
 //    @ResponseBody
-    public String checkLogin(@RequestParam("idEmp") String id, @RequestParam("passEmp") String password, HttpSession session,HttpServletRequest request,HttpServletResponse response) {
+    public String checkLogin(@RequestParam("idEmp") String id, @RequestParam("passEmp") String password, HttpSession session, HttpServletRequest request, HttpServletResponse response) {
         if (daoEmp.findById(id) != null) {
             Employee employee = daoEmp.findById(id);
             if (BCrypt.checkpw(password, employee.getPassword())) {
@@ -107,7 +124,7 @@ public class MainController {
                 }
                 Iterable<Employeerole> employeeroles = daoEmpR.findAll();
                 for (Employeerole data : employeeroles) {
-                    if (data.getEmployee().getId().equalsIgnoreCase(id)&&data.getRole().getId().equalsIgnoreCase("CVR0")) {
+                    if (data.getEmployee().getId().equalsIgnoreCase(id) && data.getRole().getId().equalsIgnoreCase("CVR0")) {
                         request.getSession().setAttribute("isAdmin", id);
                     }
                 }
@@ -117,7 +134,7 @@ public class MainController {
                         request.getSession().setAttribute("isParticipant", id);
                     }
                 }
-                
+
                 return "redirect:/dashboard";
             }
         }
@@ -127,7 +144,7 @@ public class MainController {
     @PostMapping("/register")
 //    @ResponseBody
     public String register(Model model) {
-        
+
         return "redirect:/register";
     }
 
@@ -135,9 +152,9 @@ public class MainController {
     public String dashboard() {
         return "/dashboard";
     }
-    
+
     @GetMapping("/activation")
-    public String activation(@RequestParam("id")String id,@RequestParam("ca")String password) {
+    public String activation(@RequestParam("id") String id, @RequestParam("ca") String password) {
         Employee emp = daoEmp.findById(id);
         if (password.equalsIgnoreCase(emp.getPassword())) {
             emp.setIsdeleted(new Short("0"));
@@ -145,7 +162,7 @@ public class MainController {
         }
         return "/dashboard";
     }
-   
+
     @GetMapping("/registration")
     public String registration(Model model) {
         model.addAttribute("dataReligion", daoR.findAll());
@@ -153,16 +170,16 @@ public class MainController {
         model.addAttribute("dataDistrict", daoLD.findAll());
         return "/register";
     }
-    
+
     @PostMapping("/insertRegister")
     public String saveEmployee(
-            @RequestParam("name")String name,@RequestParam("birthplace")String birthplace
-            ,@RequestParam("birthdate")String birthdate,@RequestParam("email")String email
-            ,@RequestParam("gender")String gender,@RequestParam("religion")String religion
-            ,@RequestParam("phone")String phone,@RequestParam("village")String village
-            ,@RequestParam("marriedstatus")String marriedstatus,@RequestParam("address")String address
-            ,@RequestParam("onboarddate")String onboarddate,@RequestParam("hiringlocation")String hiringlocation
-    ){
+            @RequestParam("name") String name, @RequestParam("birthplace") String birthplace,
+             @RequestParam("birthdate") String birthdate, @RequestParam("email") String email,
+             @RequestParam("gender") String gender, @RequestParam("religion") String religion,
+             @RequestParam("phone") String phone, @RequestParam("village") String village,
+             @RequestParam("marriedstatus") String marriedstatus, @RequestParam("address") String address,
+             @RequestParam("onboarddate") String onboarddate, @RequestParam("hiringlocation") String hiringlocation
+    ) {
         try {
             Employee emp = new Employee("-", name, dateFormat.parse(birthdate),
                     new Short(gender), new Short(marriedstatus), address, email,
@@ -174,7 +191,7 @@ public class MainController {
             daoEmp.save(emp);
             for (Employee data : daoEmp.findAll()) {
                 if (data.getEmail().equalsIgnoreCase(email)) {
-                    mailService.sendMail(email, "New Account for "+data.getName(), "Congratulation...", data.getName(), "Your account has been created. Please activation your account by click this link.","http://localhost:8083/activation?id="+data.getId()+"&ca="+data.getPassword());
+                    mailService.sendMail(email, "New Account for " + data.getName(), "Congratulation...", data.getName(), "Your account has been created. Please activation your account by click this link.", "http://localhost:8083/activation?id=" + data.getId() + "&ca=" + data.getPassword());
                 }
             }
         } catch (Exception ex) {
@@ -182,13 +199,12 @@ public class MainController {
         }
         return "redirect:/registration";
     }
-    
+
     @GetMapping("/upload")
     public String upload() {
         return "/upload";
     }
-    
- 
+
     @PostMapping("/uploadFile")
     public String uploadFile(@RequestParam("file") MultipartFile file) {
         Employee employee = DBFileStorageService.storeFile(file);
@@ -210,18 +226,17 @@ public class MainController {
 //                .map(file -> uploadFile(file))
 //                .collect(Collectors.toList());
 //    }
-    
-     @GetMapping("/downloadFile/{fileId}")
+    @GetMapping("/downloadFile/{fileId}")
     public ResponseEntity<Resource> downloadFile(@PathVariable String fileId) {
         // Load file from database
-         Employee employee = DBFileStorageService.getFile(fileId);
+        Employee employee = DBFileStorageService.getFile(fileId);
 
         return ResponseEntity.ok()
                 .contentType(MediaType.IMAGE_JPEG)
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + employee.getName() + "\"")
                 .body(new ByteArrayResource(employee.getPhoto()));
     }
-    
+
     @GetMapping("/lihatFile")
     public ResponseEntity<byte[]> getImage() throws IOException {
         Employee employee = DBFileStorageService.getFile("14304");
@@ -231,16 +246,16 @@ public class MainController {
                 .contentType(MediaType.IMAGE_JPEG)
                 .body(employee.getPhoto());
     }
-    
+
     @GetMapping("/logout")
-    public String logout(HttpServletRequest request){
+    public String logout(HttpServletRequest request) {
         request.getSession().removeAttribute("login");
         request.getSession().removeAttribute("isAdmin");
         request.getSession().removeAttribute("isTrainer");
         request.getSession().removeAttribute("isParticipant");
         return "redirect:/";
     }
-    
+
     //    Employee Role
     @GetMapping("/employeerole")
     public String employeerole(Model m) {
@@ -250,56 +265,83 @@ public class MainController {
 //        m.addAttribute("emproledelete", new Employeerole());
         return "employeerole";
     }
-    
-    @RequestMapping(value="/emprolesave", method = RequestMethod.POST) //@PostMapping("/regionsave")
+
+    @RequestMapping(value = "/emprolesave", method = RequestMethod.POST) //@PostMapping("/regionsave")
     public String save(@ModelAttribute("emprolesave") Employeerole employeerole) {
         daoEmpR.save(employeerole);
         return "redirect:/employeerole";
     }
-    
-    @RequestMapping(value="/emproleedit", method = RequestMethod.POST) //@PostMapping("/regionsave")
+
+    @RequestMapping(value = "/emproleedit", method = RequestMethod.POST) //@PostMapping("/regionsave")
     public String edit(@ModelAttribute("emproleedit") Employeerole employeerole) {
         daoEmpR.save(employeerole);
         return "redirect:/employeerole";
     }
-    
-    @RequestMapping(value="/emproledelete", method = RequestMethod.POST) //@PostMapping("/regiondelete")
+
+    @RequestMapping(value = "/emproledelete", method = RequestMethod.POST) //@PostMapping("/regiondelete")
     public String delete(@ModelAttribute("emproleedit") Employeerole employeerole) {
         daoEmpR.delete(employeerole.getId());
         return "redirect:/employeerole";
     }
-    
-    @RequestMapping(value="/emproledelete/{id}", method = RequestMethod.GET) //@PostMapping("/regiondelete")
+
+    @RequestMapping(value = "/emproledelete/{id}", method = RequestMethod.GET) //@PostMapping("/regiondelete")
     public ModelAndView delete(@PathVariable String id, ModelMap model) {
         daoEmpR.delete(id);
         return new ModelAndView("redirect:/employeerole");
     }
 //    ENDOF Employee Role
-    
+
 //    Employee Language
     @GetMapping("/employeelanguage")
     public String employeelanguage(Model m) {
         m.addAttribute("emplangData", daoEmpL.findAll());
-        
+
         return "employeelanguage";
     }
 //    ENDOF Employee Language
-    
+
 //    Employee Skill
     @GetMapping("/employeeskill")
     public String employeeskill(Model m) {
         m.addAttribute("empskillData", daoEmpS.findAll());
-        
+
         return "employeeskill";
     }
 //    ENDOF Employee Skill
-    
+
 //    Work Experience
     @GetMapping("/workexperience")
     public String workexperience(Model m) {
         m.addAttribute("workexpData", daoW.findAll());
-        
+
         return "workexperience";
     }
 //    ENDOF Work Experience
+
+    @GetMapping("/cv")
+    public String cv(Model m) {
+        m.addAttribute("achievementData", adao.findAll());
+        m.addAttribute("achievementsave", new Achievement());
+        m.addAttribute("achievementedit", new Achievement());
+        m.addAttribute("achievementdelete", new Achievement());
+        m.addAttribute("organizationData", odao.findAll());
+        m.addAttribute("organizationsave", new Organization());
+        m.addAttribute("organizationedit", new Organization());
+        m.addAttribute("organizationdelete", new Organization());
+        m.addAttribute("educationData", edao.findAll());
+        m.addAttribute("educationsave", new Educationhistory());
+        m.addAttribute("educationedit", new Educationhistory());
+        m.addAttribute("educationdelete", new Educationhistory());
+        m.addAttribute("certificateData", cdao.findAll());
+        m.addAttribute("certificatesave", new Employeecertification());
+        m.addAttribute("certificateedit", new Employeecertification());
+        m.addAttribute("certificatedelete", new Employeecertification());
+        return "/cv";
+    }
+
+    @RequestMapping(value = "/achievementsave", method = RequestMethod.POST)  //@PostMapping("/regionsave")
+    public String savee(@RequestParam("sid") String id, @RequestParam("sname") String name) {
+        adao.save(new Achievement(id, name));
+        return "redirect:/cv";
+    }
 }
