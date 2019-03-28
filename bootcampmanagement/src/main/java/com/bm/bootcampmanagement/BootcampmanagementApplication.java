23136e0c2243421472b45e5dd69fb19c1a3a1f8c
gerydanu <gerydanu@live.com>
2019-03-28 19:08:48 +0700

    yuuhuu

--- bootcampmanagement/src/main/java/com/bm/bootcampmanagement/BootcampmanagementApplication.java
+++ bootcampmanagement/src/main/java/com/bm/bootcampmanagement/BootcampmanagementApplication.java
@@ -10,15 +10,16 @@ public class BootcampmanagementApplication {
         SpringApplication.run(BootcampmanagementApplication.class, args);
 //        System.out.println("UNLIMITED FULL BRUST!!!");
         System.out.println("                                                                                                                 ");
-        System.out.println("  PT. Mitra Integrasi Informatika                                                                                ");
+        System.out.println("  Hello! Welcome to...                                                                                           ");
         System.out.println("  ____              _                              __  __                                                   _    ");
-        System.out.println(" |  _ \\            | |                            |  \\/  |                                                 | |   ");
+        System.out.println(" |  _ \\            | |                            |  \\/  |    PT. Mitra Integrasi Informatika              | |   ");
         System.out.println(" | |_) | ___   ___ | |_ ___ __ _ _ __ ___  _ __   | \\  / | __ _ _ __   __ _  __ _  ___ _ __ ___   ___ _ __ | |_  ");
         System.out.println(" |  _ < / _ \\ / _ \\| __/ __/ _` | '_ ` _ \\| '_ \\  | |\\/| |/ _` | '_ \\ / _` |/ _` |/ _ \\ '_ ` _ \\ / _ \\ '_ \\| __| ");
         System.out.println(" | |_) | (_) | (_) | || (_| (_| | | | | | | |_) | | |  | | (_| | | | | (_| | (_| |  __/ | | | | |  __/ | | | |_  ");
         System.out.println(" |____/ \\___/ \\___/ \\__\\___\\__,_|_| |_| |_| .__/  |_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_| |_| |_|\\___|_| |_|\\__| ");
         System.out.println("                                          | |                                __/ |                               ");
         System.out.println("                                          |_|                               |___/                                ");
+        System.out.println("                                                                                                                 ");
     }
 
 }
